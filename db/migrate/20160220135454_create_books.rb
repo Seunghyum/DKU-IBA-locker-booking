2d62eea9d76bdb19594bd4ff00f138cec1ee61b6
Moon SeungHyun <seunghyun257@naver.com>
2016-02-22 04:32:16 +0000

    add seed file - user and add db - giver column

--- db/migrate/20160220135454_create_books.rb
+++ db/migrate/20160220135454_create_books.rb
@@ -3,8 +3,11 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :title
       t.string :class
+      t.string :giver
+      
       t.integer :year
       t.integer :bnum
+      
       t.references :user, index: true
       t.references :major, index: true
       t.references :liberal, index: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,46 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
  
-User.create(email: "1234@1234.com", username: "문승현", user_major: "국제경영", password: "12341234")
-User.create(email: "12@12.com", username: "연습1", user_major: "국제경영", password: "12341234")
-User.create(email: "23@23.com", username: "연습2", user_major: "국제경영", password: "12341234")
-User.create(email: "34@34.com", username: "연습3", user_major: "국제경영", password: "12341234")
-User.create(email: "45@45.com", username: "연습4", user_major: "국제경영", password: "12341234")
-User.create(email: "67@67.com", username: "연습6", user_major: "국제경영", password: "12341234")
-User.create(email: "78@78.com", username: "연습7", user_major: "국제경영", password: "12341234")
-User.create(email: "89@89.com", username: "연습8", user_major: "국제경영", password: "12341234")
-User.create(email: "10@10.com", username: "연습9", user_major: "국제경영", password: "12341234")
-User.create(email: "11@11.com", username: "연습10", user_major: "국제경영", password: "12341234")
-User.create(email: "12@12.com", username: "연습11", user_major: "국제경영", password: "12341234")
-User.create(email: "123@123.com", username: "연습12", user_major: "국제경영", password: "12341234")
+# User.create(email: "1234@1234.com", username: "문승현", user_major: "국제경영", password: "12341234")
+# User.create(email: "12@12.com", username: "연습1", user_major: "국제경영", password: "12341234")
+# User.create(email: "23@23.com", username: "연습2", user_major: "국제경영", password: "12341234")
+# User.create(email: "34@34.com", username: "연습3", user_major: "국제경영", password: "12341234")
+# User.create(email: "45@45.com", username: "연습4", user_major: "국제경영", password: "12341234")
+# User.create(email: "67@67.com", username: "연습6", user_major: "국제경영", password: "12341234")
+# User.create(email: "78@78.com", username: "연습7", user_major: "국제경영", password: "12341234")
+# User.create(email: "89@89.com", username: "연습8", user_major: "국제경영", password: "12341234")
+# User.create(email: "10@10.com", username: "연습9", user_major: "국제경영", password: "12341234")
+# User.create(email: "11@11.com", username: "연습10", user_major: "국제경영", password: "12341234")
+# User.create(email: "12@12.com", username: "연습11", user_major: "국제경영", password: "12341234")
+# User.create(email: "123@123.com", username: "연습12", user_major: "국제경영", password: "12341234")
 
+
+user = User.new(
+      :major_id              => 2,
+      :email                 => "12341234@dankook.ac.kr",
+      :password              => "12341234",
+      :password_confirmation => "12341234",
+      :username          => "김우정",
+      gender: false,
+      user_student_num: 12341234,
+      my_locker: 0
+      
+                )
+user.skip_confirmation!
+user.save!
+
+user = User.new(
+      :major_id              => 1,
+      :email                 => "23452345@dankook.ac.kr",
+      :password              => "12341234",
+      :password_confirmation => "12341234",
+      :username          => "문승현",
+      gender: true,
+      user_student_num: 23452345,
+      my_locker: 0
+                )
+user.skip_confirmation!
+user.save!
 
 Major.create(mname: "국제경영")
 Major.create(mname: "모바일")
