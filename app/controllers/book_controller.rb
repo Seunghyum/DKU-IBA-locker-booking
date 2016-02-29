90293069e21231ba8f75888d4b9993c8ff0ed634
Moon SeungHyun <seunghyun257@naver.com>
2016-02-29 19:15:35 +0000

    Add flush message and Fix controller error

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -10,21 +10,23 @@ class BookController < ApplicationController
 #책 선택 로직
   def selecting
       
-      @mybook = Book.where(user_id: current_user.id, btitle: @btitle).take
+    @mybook = Book.where(user_id: current_user.id, btitle: @btitle).take
       
-    if @selecting_book.user_id.nil? && @mybook != @selecting_book 
+    #책의 대여자가 없을 때 + 만일 같은이름의 책을 가지고 있다면 가지지 못하게.
+    if @selecting_book.user_id.nil? && @mybook ? @mybook.btitle != @selecting_book.btitle : true 
       # current_user.update( book_id: @set_book )
       @selecting_book.update( user_id: current_user.id )
-      
-      redirect_to :action => "index"          
+      flash[:success] = "#{@selecting_book.btitle} 책을 신청했습니다."
+      redirect_to book_index_path        
     else
-      redirect_to :action => "reject"
+      flash[:danger] = "같은 책을 2권이상 신청 할 수 없습니다."
+      redirect_to book_index_path
     end
   end
   
   def destroy
     @selecting_book.update( user_id: nil)
-    
+    flash[:danger] = '책 신청권을 포기했습니다.'
     redirect_to book_index_path
   end
   
--- app/controllers/locker_controller.rb
+++ app/controllers/locker_controller.rb
@@ -7,18 +7,20 @@ class LockerController < ApplicationController
   def index
   end
   
-#첫번째 번호표 뽑기 로직
+#1차접수 번호표 뽑기 로직
   def first_check
     
-    #2번 번호가 제한을 넘는지 확인하는 과정
+    #1차 접수 - 제한을 넘는지 확인하는 과정
     current_user.update(my_num: @numbering)
     current_user.major.update(locker_numbering: @numbering)
     if current_user.locker.nil? && current_user.my_num < current_user.major.locker_limit
       Locker.create(user_id: current_user.id, major_id: current_user.major_id)
       current_user.update(locker_id: Locker.where(user_id: current_user.id).take.id)
       
+      flash[:success] = "1차 접수 - 총 제한 인원 #{current_user.major.locker_limit}명 중 #{current_user.my_num}번째 입니다. 사물함을 선택해주세요"
       redirect_to :action => "selecting"
     else  
+      flash[:danger] = "1차 접수 - 총 제한 인원 #{current_user.major.locker_limit}명 중 #{current_user.my_num}번째 입니다. 다음학기에....."
       redirect_to :action => "reject"
     end
   end
@@ -31,9 +33,13 @@ class LockerController < ApplicationController
   def lockerselect
     if current_user.locker.lnum == 0  || current_user.my_num <= current_user.major.locker_limit#현재 유저가 사물함 가진것이 없으나 사물함 얻을 수 있는 번호 일 때
       @mylocker.update(lnum: @lnum)
-      redirect_to :action => "manage"
+      # redirect_to locker_index_path, notice: "총 제한 인원 #{current_user.major.locker_limit}명 중 #{current_user.my_num}번째 입니다. 사물함을 선택해주세요"
+      flash[:success] = "#{@lnum}번 사물함을 획득했습니다."
+      redirect_to locker_index_path
     elsif current_user.my_num >= current_user.major.locker_limit#사물함 제한 숫자에 들지 못한 유저가 어쩌다가 이 페이지로 왔을 떄
-      redirect_to :action => "reject"
+      #redirect_to locker_index_path, notice: "총 제한 인원 #{current_user.major.locker_limit}명 중 #{current_user.my_num}번째 입니다. 다음학기에..... "
+      flash[:danger] = "다른 사람이 이미 점유했습니다. 비어있는 다른 사물함을 다시 골라주세요!"
+      redirect_to locker_index_path
     end
   end
   
